8c922c2a236b9d0f842fd039189f46a851824d4b
Ricardo <rivel_co@hotmail.com>
2017-01-03 21:05:11 -0600

    Corrección Background Shift

--- JS/Universal/BackgroundShift.js
+++ JS/Universal/BackgroundShift.js
@@ -1,20 +1,20 @@
 //var fondos = ['url("../Multimedia/Fondos/01.jpg")'];
-var fondos = ['url(../Recursos/Multimedia/Fondos/01.jpg)',
-	'url(../Recursos/Multimedia/Fondos/02.jpg)',
-	'url(../Recursos/Multimedia/Fondos/03.jpg)',
-	'url(../Recursos/Multimedia/Fondos/04.jpg)',
-	'url(../Recursos/Multimedia/Fondos/05.jpg)',
-	'url(../Recursos/Multimedia/Fondos/06.jpg)',
-	'url(../Recursos/Multimedia/Fondos/07.jpg)',
-	'url(../Recursos/Multimedia/Fondos/08.jpg)',
-	'url(../Recursos/Multimedia/Fondos/09.jpg)',
-	'url(../Recursos/Multimedia/Fondos/10.jpg)',
-	'url(../Recursos/Multimedia/Fondos/11.jpg)',
-	'url(../Recursos/Multimedia/Fondos/12.jpg)',
-	'url(../Recursos/Multimedia/Fondos/13.jpg)',
-	'url(../Recursos/Multimedia/Fondos/14.jpg)',
-	'url(../Recursos/Multimedia/Fondos/15.jpg)',
-	'url(../Recursos/Multimedia/Fondos/16.jpg)'
+var fondos = ['url(http://www.include-poetry.com/IP-Sources/Multimedia/Fondos/01.jpg)',
+	'url(http://www.include-poetry.com/IP-Sources/Multimedia/Fondos/02.jpg)',
+	'url(http://www.include-poetry.com/IP-Sources/Multimedia/Fondos/03.jpg)',
+	'url(http://www.include-poetry.com/IP-Sources/Multimedia/Fondos/04.jpg)',
+	'url(http://www.include-poetry.com/IP-Sources/Multimedia/Fondos/05.jpg)',
+	'url(http://www.include-poetry.com/IP-Sources/Multimedia/Fondos/06.jpg)',
+	'url(http://www.include-poetry.com/IP-Sources/Multimedia/Fondos/07.jpg)',
+	'url(http://www.include-poetry.com/IP-Sources/Multimedia/Fondos/08.jpg)',
+	'url(http://www.include-poetry.com/IP-Sources/Multimedia/Fondos/09.jpg)',
+	'url(http://www.include-poetry.com/IP-Sources/Multimedia/Fondos/10.jpg)',
+	'url(http://www.include-poetry.com/IP-Sources/Multimedia/Fondos/11.jpg)',
+	'url(http://www.include-poetry.com/IP-Sources/Multimedia/Fondos/12.jpg)',
+	'url(http://www.include-poetry.com/IP-Sources/Multimedia/Fondos/13.jpg)',
+	'url(http://www.include-poetry.com/IP-Sources/Multimedia/Fondos/14.jpg)',
+	'url(http://www.include-poetry.com/IP-Sources/Multimedia/Fondos/15.jpg)',
+	'url(http://www.include-poetry.com/IP-Sources/Multimedia/Fondos/16.jpg)'
 ];
 
 var x = Math.floor((Math.random() * fondos.length) + 1);
