c196a6e09418977ebb5d5a9d6cadd9485c8bb73f
Ricardo <rivel_co@hotmail.com>
2017-08-05 20:32:08 -0500

    Mejoras y optimizaciones

--- JS/Course/Loader.js
+++ JS/Course/Loader.js
@@ -1,17 +1,3 @@
-// Función recursiva para animación tipewriter
-function typeWriter(target, text, n) {
-	// target = elemento donde se escribirá
-	// text = texto a escribir
-	// n = letra por la cual empezar
-	if (n < (text.length)) {
-		$(target).html(text.substring(0, n+1));
-		n++;
-		setTimeout(function() {
-			typeWriter(target, text, n)
-		}, 100); // Retardo entre letra y letra
-	}
-}
-
 // Función de loader, usa el archivo jpreloader.min_.js
 $(document).ready(function(e) {
 	$("#IconosMenu,#main,footer").css("display", "none");
@@ -24,33 +10,6 @@ $(document).ready(function(e) {
 		$(".LoaderGif").fadeOut(400, function(){
 			$(".LoadingFondo").fadeOut(); // Gif loading
 		}); // Fondo negro
-		
-		if ($("#TituloIP").length > 0){
-			$("#IconosMenu,#main,footer").fadeIn(400, function(){
-				var VarInc = $('cur').attr('include');
-				var RetInc = VarInc.length * 100;
-				setTimeout(function(){ // Animación typewriter
-					typeWriter('#Gato', '#', 0);
-					setTimeout(function(){
-						typeWriter('n', 'include', 0);
-						setTimeout(function(){
-							document.getElementById("s1").innerHTML = "<";
-							setTimeout(function(){
-								document.getElementById("s2").innerHTML = ">";
-								setTimeout(function(){
-									typeWriter('cur', VarInc, 0);
-									setTimeout(function(){
-										$('#IntroGrl').fadeIn('slow');
-									}, RetInc);
-								}, 500);
-							}, 300);
-						}, 900);
-					}, 200);
-				}, 500);
-			});
-		} else {
-			$("#IconosMenu,#main,footer").fadeIn();
-		}	
+		$("#IconosMenu,#main,footer").fadeIn();
 	});
 });
-
--- JS/Universal/DarkMode.js
+++ JS/Universal/DarkMode.js
@@ -11,45 +11,28 @@ function DarkMode(){
 		picsec = 'B.png';
 
 	$('h1, h2, h3, h4, h5, h6, h2 a').css('color', blanco);
-	$('p, p a, blockquote, ul, ol, ul a, #RegisterForm, #RegisterForm input, #RegisterForm select, #main p a:not(.TagPost), .Nav a, #ComentAct h2, .MJXc-display').css('color', blanco2); 
+	$('#main p, #main dl, #main p a, #main blockquote, #main ul, #main ol, #main ul a, #main p a:not(.TagPost)').css('color', blanco2); 
+	$('.Nav a, #ComentAct h2, .MJXc-display').css('color', blanco2);
 	$('html').css('background-color', oscuro1);
 	$('.CajaComent').css('background-color', comentarios);
 	$('blockquote').css('border-left-color', blanco);
-	$('code, .TagPost, #RegisterForm input, #RegisterForm select').css({
+	$('code, .TagPost').css({
 		'background-color': oscuro2,
 		'border-color': oscuro3,
 	});
-	$('.HOficial, .HOficial section a').css({
-		'color': blanco,
-		'background-color': '#131313'
-	});
-	$('.btn-multi .btn').css({
-		'color': oscuro2,
-		'background-color': blanco
-	});
-	$('.btn-multi a, .btn-multi label > .icon').css('color', oscuro2);
-	$('body section article nav a').css('color', blanco2);
-	if ($('blockquote').css('background-color') == 'rgb(249, 249, 249)'){
-		$('blockquote').css({
-			'background-color': '#222',
-			'border-left-color': '#666'
-		});
-	}
-	$('#RegisterForm span').css('color', '#F92672');
 	$('table').css({
 		'background-color': oscuro4,
 		'color': blanco2,
 	});
-	$('#Captcha').attr({
-		'data-theme': 'dark'
-	});
 	$('table tr:nth-child(even)').css('background-color', oscuro2);
+
 	var src = $('#LogoMenuN').attr("src").replace(picori, picsec);
 	$('#LogoMenuN').attr("src", src);
 	var srcMin = $('#LMN').attr("src").replace(picori, picsec);
 	$('#LMN').attr("src", srcMin);
 	$('#MenuBars em').css('background', blanco);
 }
+
 var running = false,
     count = 0,
     sigue = true;
@@ -59,9 +42,7 @@ var n = d.getHours();
 
 if (n > 18 || n < 7){
 	sigue = false;
-	if (! $('#TituloIP').is(':visible')){
-		DarkMode();
-	}
+	DarkMode();
 }
 
 var end_counter = function() {
