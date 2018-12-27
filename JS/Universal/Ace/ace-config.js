2085edaea38127d4397e5666394bcaae07a4f118
Ricardo Velázquez Contreras <rivel_co@hotmail.com>
2018-12-26 19:11:06 -0600

    New Ace config

--- JS/Universal/Ace/ace-config.js
+++ JS/Universal/Ace/ace-config.js
@@ -1,10 +1,10 @@
-var editor, salida;
+var editor, salida, eKarel;
 var fuenteSize = '1';
 if ($(window).width() < 600) {
 	fuenteSize = '0.8';
 }
 
-$('.editor').each(function( index ) {
+$('textarea.cpp').each(function( index ) {
 	editor = ace.edit(this);
 	ace.require('ace/ext/settings_menu').init(editor);
 	editor.setTheme("ace/theme/monokai");
@@ -24,7 +24,28 @@ $('.editor').each(function( index ) {
 	}]);
 });
 
-$('.eKarel').each(function( index ) {
+$('textarea.output').each(function( index ) {
+	editor = ace.edit(this);
+	ace.require('ace/ext/settings_menu').init(editor);
+	editor.setTheme("ace/theme/monokai");
+	editor.session.setMode("ace/mode/plain_text");
+	editor.setShowPrintMargin(false);
+	editor.setOptions({
+	    maxLines: 20,
+	    fontSize: fuenteSize + 'em'
+	});
+	editor.commands.addCommands([{
+		name: "showSettingsMenu",
+		bindKey: {win: "Ctrl-q", mac: "Command-q"},
+		exec: function(editor) {
+			editor.showSettingsMenu();
+		},
+		readOnly: true
+	}]);
+	editor.setReadOnly(true);
+});
+
+$('textarea.karel').each(function( index ) {
 	eKarel = ace.edit(this);
 	ace.require('ace/ext/settings_menu').init(eKarel);
 	eKarel.setTheme("ace/theme/karel");
@@ -44,11 +65,74 @@ $('.eKarel').each(function( index ) {
 	}]);
 });
 
-$('.output').each(function(index) {
+$('textarea.plain-text').each(function(index) {
 	salida = ace.edit(this);
 	ace.require('ace/ext/settings_menu').init(salida);
 	salida.setTheme("ace/theme/monokai");
 	salida.session.setMode("ace/mode/plain_text");
+	salida.setShowPrintMargin(false);
+	salida.setOptions({
+	    maxLines: 10,
+	    fontSize: fuenteSize + 'em'
+	});
+	salida.commands.addCommands([{
+		name: "showSettingsMenu",
+		bindKey: {win: "Ctrl-q", mac: "Command-q"},
+		exec: function(salida) {
+			salida.showSettingsMenu();
+		},
+		readOnly: true
+	}]);
+});
+
+$('textarea.html').each(function(index) {
+	salida = ace.edit(this);
+	ace.require('ace/ext/settings_menu').init(salida);
+	salida.setTheme("ace/theme/monokai");
+	salida.session.setMode("ace/mode/html");
+	salida.session.setUseWorker(false);
+	salida.setShowPrintMargin(false);
+	salida.setOptions({
+	    maxLines: 10,
+	    fontSize: fuenteSize + 'em'
+	});
+	salida.commands.addCommands([{
+		name: "showSettingsMenu",
+		bindKey: {win: "Ctrl-q", mac: "Command-q"},
+		exec: function(salida) {
+			salida.showSettingsMenu();
+		},
+		readOnly: true
+	}]);
+});
+
+$('textarea.markdown').each(function(index) {
+	salida = ace.edit(this);
+	ace.require('ace/ext/settings_menu').init(salida);
+	salida.setTheme("ace/theme/monokai");
+	salida.session.setMode("ace/mode/markdown");
+	salida.session.setUseWorker(false);
+	salida.setShowPrintMargin(false);
+	salida.setOptions({
+	    maxLines: 10,
+	    fontSize: fuenteSize + 'em'
+	});
+	salida.commands.addCommands([{
+		name: "showSettingsMenu",
+		bindKey: {win: "Ctrl-q", mac: "Command-q"},
+		exec: function(salida) {
+			salida.showSettingsMenu();
+		},
+		readOnly: true
+	}]);
+});
+
+$('textarea.python').each(function(index) {
+	salida = ace.edit(this);
+	ace.require('ace/ext/settings_menu').init(salida);
+	salida.setTheme("ace/theme/monokai");
+	salida.session.setMode("ace/mode/python");
+	salida.session.setUseWorker(false);
 	salida.setShowPrintMargin(false);
 	salida.setOptions({
 	    maxLines: 10,
